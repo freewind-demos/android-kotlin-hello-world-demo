e6e6431477a62bdd72f2c23d7866df459504c80a
Freewind <nowindlee@gmail.com>
2021-12-31 19:13:11 +0800

    simplify

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 Android Kotlin Hello World Project
 ===========================
 
-Run this demo from your IDE.
+一个最简单的Kotlin Android项目。
+
+Run `MainActivity` from your IDE.
 
 注：无法通过命令行`./gradlew installDebug`部署
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,9 +16,7 @@ android {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation("com.google.dagger:dagger:2.40.5")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
     implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.2")
-    annotationProcessor("com.google.dagger:dagger-compiler:2.40.5")
 }
